82a941f65565e953e2f9462911976cc390ec300e
Igor Simões <igorsimoes11.06@gmail.com>
2025-03-04 12:54:45 +0000

    Accept requests with over 4KB of data

--- erlang_dynamodb_service/src/gpb_tcp_protocol.erl
+++ erlang_dynamodb_service/src/gpb_tcp_protocol.erl
@@ -7,6 +7,7 @@
 -include("kv_pb.hrl").
 
 -define(SOCKET_TIMEOUT, 5000).
+-define(MAX_PACKET_SIZE, 6143).
 
 start_link(Ref, Transport, Opts) ->
     Pid = spawn_link(?MODULE, init, [Ref, Transport, Opts]),
@@ -15,15 +16,23 @@ start_link(Ref, Transport, Opts) ->
 init(Ref, Transport, _Opts) ->
     {ok, Socket} = ranch:handshake(Ref),
     %% If you want manual recv, set the socket in passive mode.
-    ok = Transport:setopts(Socket, [{active, false}]),
-    loop(Socket, Transport).
+    ok = Transport:setopts(Socket, [{active, false}, {buffer, 8192}, {recbuf, 8192}]),
+    loop(Socket, Transport, <<>>).
 
-loop(Socket, Transport) ->
-    %% Synchronously receive data with a 5s timeout.
+loop(Socket, Transport, Buffer) ->
     case Transport:recv(Socket, 0, ?SOCKET_TIMEOUT) of
         {ok, Bin} ->
-            handle_request(Bin, Socket, Transport),
-            loop(Socket, Transport);
+            NewBuffer = <<Buffer/binary, Bin/binary>>,
+            case decode_and_process(NewBuffer, Socket, Transport) of
+                {ok, EncBin} ->
+                    Transport:send(Socket, EncBin),
+                    loop(Socket, Transport, <<>>);
+                {error, Reason, Error} ->
+                    io:format("Error processing data: ~p~n", [Reason]),
+                    Transport:send(Socket, Error),
+                    loop(Socket, Transport, <<>>)
+            end;
+
         {error, closed} ->
             io:format("Client disconnected~n", []),
             ok;
@@ -32,28 +41,42 @@ loop(Socket, Transport) ->
             Transport:close(Socket)
     end.
 
-handle_request(Bin, Socket, Transport) ->
-    Decoded = kv_pb:decode_msg(Bin, req_envelope),
-    case Decoded of
+decode_and_process(Buffer, Socket, Transport) ->
+    case gpb:decode_packet(uint32, Buffer, [{packet_size, ?MAX_PACKET_SIZE}]) of
+        {ok, PacketBin, _Rest} ->
+            decode_msg(PacketBin);
+        {more, _TotalSizeNeeded} ->
+            loop(Socket, Transport, Buffer);
+        {error, Reason} ->
+            {error, Reason, <<"PacketFrameError">>}
+    end.
+
+decode_msg(MsgBin) ->
+    case kv_pb:decode_msg(MsgBin, req_envelope, []) of
         Envelope when is_record(Envelope, req_envelope) ->
             ResponseEnvelope = process_envelope(Envelope),
             EncBin = kv_pb:encode_msg(ResponseEnvelope, req_envelope, []),
-            Transport:send(Socket, EncBin);
-        {error, Reason} ->
-            io:format("Protobuf decode error with reason: ~p~n", [Reason]),
-            Transport:send(Socket, <<"DecodeError">>);
-        Error ->
-            io:format("Protobuf decode error: ~p~n", [Error]),
-            Transport:send(Socket, <<"DecodeError">>)
+            {ok, EncBin};
+
+        {error, DecodeReason} ->
+            {error, DecodeReason, <<"ProtobufDecodeError">>}
     end.
 
 process_envelope(#req_envelope{type = set_request_t, set_req = SetReq}) ->
     #set_request{req = #data{key=Key, value=Val}} = SetReq,
-    ok = ddb_client:put_item(Key, Val),
-    #req_envelope{
-       type = set_response_t,
-       set_resp = #set_response{error = ok}
-    };
+
+    case ddb_client:put_item(Key, Val) of
+        ok ->
+            #req_envelope{
+              type = set_response_t,
+              set_resp = #set_response{error = ok}
+            };
+        {error, _} ->
+            #req_envelope{
+              type = set_response_t,
+              set_resp = #set_response{error = internal}
+            }
+    end;
 
 process_envelope(#req_envelope{type = get_request_t, get_req = GetReq}) ->
     #get_request{key=Key} = GetReq,
--- erlang_dynamodb_service/test/test_client.erl
+++ erlang_dynamodb_service/test/test_client.erl
@@ -6,6 +6,7 @@
 -define(HOST, "localhost").
 -define(PORT, 8080).
 -define(SOCKET_TIMEOUT, 5000).
+-define(MAX_PACKET_SIZE, 6143).
 
 set_kv(Key, Value) ->
   ReqEnvelope = build_req_envelope(
@@ -32,19 +33,37 @@ build_req_envelope(Type, Request) ->
   #req_envelope{type = Type, set_req = SetReq, get_req = GetReq}.
 
 do_request(Envelope) ->
-    Sock = connect(),
-    EncBin = encode_response(Envelope),
-    ok = gen_tcp:send(Sock, EncBin),
-    receive_response(Sock).
+    case encode_request(Envelope) of
+      {ok, EncBin} ->
+        Sock = connect(),
+        ok = gen_tcp:send(Sock, EncBin),
+        receive_response(Sock);
+      {error, too_large} ->
+        io:format("Request too large. Packet max size is: ~p~n", [?MAX_PACKET_SIZE]),
+        {error, too_large};
+      Error ->
+        io:format("Error encoding request: ~p~n", [Error]),
+        Error
+    end.
 
 connect() ->
     {ok, Sock} = gen_tcp:connect(?HOST, ?PORT,
                                 [binary, {active, false}, {packet, 0}]),
     Sock.
 
-encode_response(Envelope) ->
-  EncBin = kv_pb:encode_msg(Envelope, req_envelope),
-  EncBin.
+encode_request(Envelope) ->
+  RawBin = kv_pb:encode_msg(Envelope, req_envelope),
+  encode_packet(RawBin).
+
+encode_packet(MsgBin) ->
+    PacketSize = byte_size(MsgBin),
+    if
+        PacketSize > ?MAX_PACKET_SIZE ->
+            {error, too_large};
+        true ->
+            VarInt = gpb:encode_varint(PacketSize),
+            {ok, <<VarInt/binary, MsgBin/binary>>}
+    end.
 
 receive_response(Sock) ->
   case gen_tcp:recv(Sock, 0, ?SOCKET_TIMEOUT) of
