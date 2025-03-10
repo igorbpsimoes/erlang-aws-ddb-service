2bc366af402b0c26d1c4f03537c981e34f4e646f
Igor Simões <igorsimoes11.06@gmail.com>
2025-03-10 01:28:45 +0000

    Cleanup cloudformation_stack script

--- cloudformation/cloudformation_stack.sh
+++ cloudformation/cloudformation_stack.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 ENVIRONMENT_NAME=${ENV_NAME}
+SCRIPT_DIR=$(dirname "$0")
 
 if [ "$1" == "deploy" ]; then
   case "$2" in
-    vpc)
-      TEMPLATE_FILE="my-vpc.yaml"
-      STACK_NAME="my-vpc-stack"
-      aws cloudformation deploy --template-file $TEMPLATE_FILE --stack-name $STACK_NAME
-      ;;
     dynamodb)
-      TEMPLATE_FILE="dynamodb_template.yaml"
+      TEMPLATE_FILE="$SCRIPT_DIR/dynamodb_template.yaml"
       STACK_NAME="${ENVIRONMENT_NAME}-dynamodb-stack"
 
       aws cloudformation deploy --template-file $TEMPLATE_FILE --stack-name $STACK_NAME \
         --capabilities CAPABILITY_NAMED_IAM \
-        --parameter-overrides EnvironmentName="${ENVIRONMENT_NAME}" \
-        KmsKeyArn="arn:aws:kms:eu-north-1:831926581541:key/41c1ee0f-af04-4344-8633-a17d1e5ee188"
+        --parameter-overrides EnvironmentName="${ENVIRONMENT_NAME}"
       ;;
     cluster)
-      TEMPLATE_FILE="cluster_template.yaml"
+      TEMPLATE_FILE="$SCRIPT_DIR/cluster_template.yaml"
       STACK_NAME="${ENVIRONMENT_NAME}-cluster-stack"
       LOCAL_IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com)
 
@@ -31,18 +26,18 @@ if [ "$1" == "deploy" ]; then
             SubnetIds="subnet-03578933c0eaa04e9" \
             LocalIp="${LOCAL_IP}"
       ;;
-    service)
-      TEMPLATE_FILE="my-service.yaml"
-      STACK_NAME="my-service-stack"
+    task)
+      TEMPLATE_FILE="$SCRIPT_DIR/task_template.yaml"
+      STACK_NAME="my-test-service-stack"
       aws cloudformation deploy --template-file $TEMPLATE_FILE --stack-name $STACK_NAME \
           --disable-rollback \
           --capabilities CAPABILITY_NAMED_IAM \
           --parameter-overrides ClusterName="MyErlangCluster" \
             TaskDefinition="arn:aws:ecs:eu-north-1:831926581541:task-definition/task-definition-erlang-service:4" \
       ;;
-    test)
+    service)
       TEMPLATE_FILE="my-service.yaml"
-      STACK_NAME="my-test-service-stack"
+      STACK_NAME="my-service-stack"
       aws cloudformation deploy --template-file $TEMPLATE_FILE --stack-name $STACK_NAME \
           --disable-rollback \
           --capabilities CAPABILITY_NAMED_IAM \
